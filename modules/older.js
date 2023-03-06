9d2292f1317835e894b437fb571d6439da5baea5
Mark hanley <home@markhanley.co.uk>
2023-03-06 14:51:30 +0000

    Refactored main.js to Task and TaskCollectionclasses

---
+++ modules/older.js
@@ -0,0 +1,95 @@
+////////////////////////////////////////////
+//creating new templates //////////////////
+/////////////////////////////////////////
+const printHTML = (input) => {
+    let template = document.createElement("template");
+    template.innerHTML = input.trim();
+    return template.content.firstElementChild;
+  };
+  
+  // new task template
+  const newItem = () => {
+    let newTask = printHTML(`
+          <div class='item width-large'>
+              <input type='checkbox' class='item__completed'</input>
+              <input type="text" class='item__description' placeholder='add item ...'></input>
+          </div>
+  `);
+    canvas.append(newTask);
+    let textInput = newTask.querySelector(".item__description");
+  
+    // event listener to add new items
+    textInput.addEventListener("keyup", (event) => {
+      enterNewItem(event, textInput);
+    });
+  };
+  
+  // local storage ///////////////////
+  ///////////////////////////////////
+  
+  // update local storage
+  function setStorage(tasks) {
+    let updatedTaskList = JSON.stringify(tasks);
+    localStorage.setItem("task-list", updatedTaskList);
+  }
+  
+  // collect from local storage
+  function retrieveStorage() {
+    let taskList = localStorage.getItem("task-list") || "[]";
+    return JSON.parse(taskList);
+  }
+  
+  //variable containing the current values in storage
+  const currentTaskList = retrieveStorage();
+  
+  //create a new task object to send to local storage
+  function newTaskObject(text, done = false) {
+      currentTaskList.unshift({
+          completed: done,
+          description: text
+      });
+  
+      setStorage(currentTaskList);
+  }
+  
+  // update tasks after being initialised
+  function updateTask() {
+  
+  }
+  
+  // initialise the page /////////////////////
+  ///////////////////////////////////////////
+  function renderTaskList() {
+    canvas.innerHTML = "";
+    newItem();
+    for (let i = 0; i < currentTaskList.length; i++) {
+      let { description, completed: done } = currentTaskList[i];
+  
+      newItem();
+      let allTaskDescriptions = document.querySelectorAll(".item__description");
+      let taskDescription = allTaskDescriptions[allTaskDescriptions.length - 1];
+      taskDescription.value = description;
+  
+      let allTasksStatus = document.querySelectorAll(".item__completed");
+      let taskDone = allTasksStatus[allTasksStatus.length - 1];
+      taskDone.checked = done;
+  
+      let newestTask = allTaskDescriptions[0];
+      newestTask.focus();
+    }
+  }
+  
+  // keyboard commands ////////////////////////
+  ////////////////////////////////////////////
+  function enterNewItem(keyPress, activeElement) {
+      if (keyPress.key === "Enter") {
+          let description = activeElement.value;
+          let done = activeElement.parentElement.querySelector("input").checked;
+          newTaskObject(description, done);
+          renderTaskList();
+      }
+  };
+  
+  // initialise the page ///////////////////////
+  /////////////////////////////////////////////
+  renderTaskList();
--- modules/task-objects.js
+++ modules/task-objects.js
@@ -1,76 +1,118 @@
 export class Task {
-    constructor(description, status = TaskStatus.Pending) {
-        this.description = description;
-        this.status = status;
-        this.template = document.createElement('template');
-    }
+  constructor(description = "", status = TaskStatus.Pending) {
+    this.description = description;
+    this.status = status;
+    this.template = document.createElement("template");
+    this.theTextField = null;
+  }
 
-    static newItemTemplate = `
+  get textInputField() {
+    const textField = this.template.content.querySelector(".item__description");
+    return textField;
+  }
+
+  focusInput() {
+    console.log(`Attempting to set focus of '${this.description}' task`);
+    
+    this.theTextField.focus();
+  }
+
+  getStatusText() {
+    return Object.keys(TaskStatus).find(
+      (key) => TaskStatus[key] === this.status
+    );
+  }
+  getNode() {
+    let checkBoxStatus = this.status === TaskStatus.Complete ? " checked" : "";
+    let newItemTemplate = `
     <div class='item'>
-        <input type='checkbox' class='item__completed'</input>
-        <input type="text" class='item__description' placeholder='add item ...'></input>
+        <input type='checkbox' class='item__completed'${checkBoxStatus}></input>
+        <input type="text" class='item__description' placeholder='add item ...' value='${this.description}'></input>
     </div>
     `;
 
-    getStatusText() {
-        return Object.keys(TaskStatus).find((key) => TaskStatus[key] === this.status);
+    this.template.innerHTML = newItemTemplate.trim();
+
+    this.textInputField.addEventListener("keyup", (event) => {
+      this.#updateItem(event, this.textInputField);
+    });
+    this.theTextField = this.textInputField;
+
+    return this.template.content.firstElementChild;
+  }
+
+  #updateItem(keyPress, inputField) {
+    console.log("In keypress event handler: " + inputField);
+    
+    if (keyPress.key === "Enter") {
+      this.description = inputField.value;
+      const done = inputField.parentElement.querySelector("input").checked;
+      this.status = done ? TaskStatus.Complete : TaskStatus.InProgress;
     }
-    getNode() {
-        this.template.innerHTML = this.newItemTemplate.trim();
-        return template.content.firstElementChild;
-    }
-    getTaskAsObject() {
-        return {
-            description: this.description,
-            status: this.status,
-        };
-    }
+  }
+
+  getTaskAsObject() {
+    return {
+      description: this.description,
+      status: this.status,
+    };
+  }
 }
 
 // TaskStatus enumeration
 export const TaskStatus = Object.freeze({
-    Pending: 0,
-    InProgress: 1,
-    Complete: 2,
+  Pending: 0,
+  InProgress: 1,
+  Complete: 2,
 });
 
 export class TaskCollection {
-    constructor() {
-        this.allTasks = [];
-    }
+  constructor() {
+    this.allTasks = [];
+  }
 
-    addTask(task) {
-        this.allTasks.push(task);
-        this.saveAllTasksToStorage();
-    }
+  addTask(task) {
+    this.allTasks.unshift(task);
+    this.saveAllTasksToStorage();
+  }
 
-    deleteTask(index) {
-        this.allTasks = this.allTasks.splice(index,1);
-        this.saveAllTasksToStorage();
-    }
+  deleteTask(index) {
+    this.allTasks = this.allTasks.splice(index, 1);
+    this.saveAllTasksToStorage();
+  }
 
-    getAllTasksFromStorage() {
-        const taskListFromStorage = localStorage.getItem('task-list') || '[]';
-        const taskListAsArray = JSON.parse(taskListFromStorage);
-        this.allTasks = [];
-        taskListAsArray.forEach((taskObjectFromStorage) => {
-            this.allTasks.push (new Task(
-                taskObjectFromStorage.description,
-                taskObjectFromStorage.status
-            ));
-        });
-    }
+  getAllTasksFromStorage() {
+    const taskListFromStorage = localStorage.getItem("task-list") || "[]";
+    const taskListAsArray = JSON.parse(taskListFromStorage);
+    this.allTasks = [];
+    taskListAsArray.forEach((taskObjectFromStorage) => {
+      this.allTasks.unshift(
+        new Task(
+          taskObjectFromStorage.description,
+          taskObjectFromStorage.status
+        )
+      );
+    });
+  }
 
-    saveAllTasksToStorage() {
-        const allTasksArray = [];
-        this.allTasks.forEach ((task) => {
-            allTasksArray.push(task.getTaskAsObject());
-        });
-        const allTasksAsJson = JSON.stringify(allTasksArray);
-        localStorage.setItem('task-list', allTasksAsJson);
-    }
+  saveAllTasksToStorage() {
+    const allTasksArray = [];
+    this.allTasks.forEach((task) => {
+      allTasksArray.unshift(task.getTaskAsObject());
+    });
+    const allTasksAsJson = JSON.stringify(allTasksArray);
+    localStorage.setItem("task-list", allTasksAsJson);
+  }
 
-    displayTasks(canvas) {
-        ;
-    }
+  displayTasks(canvas) {
+    canvas.innerHTML = "";
+    this.allTasks.forEach((task, index) => {
+      const node = task.getNode();
+        if (index === 0 ) {
+            task.textInputField.focus();
+        }
+
+      canvas.append(node);
+    });
+  }
 }
